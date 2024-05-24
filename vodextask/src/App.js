53ab0179fc5ebde8328544a48a5f85216b4b4348
shobhit0111 <shobhitbhatnagar61@gmail.com>
2024-05-24 13:39:07 +0530

    Home1 added

--- vodextask/src/App.js
+++ vodextask/src/App.js
@@ -1,39 +1,10 @@
-import logo from './Images/logo.png';
-import Background from './Images/Background1.jpeg';
+import React from 'react'
+import Home1 from './Components/Home1/Home1'
 
-import './App.css';
-
-const Header = () =>{
-  return(
+export default function App() {
+  return (
     <>
-      <div className="Header">
-        <div className="logo-container">
-          <img src={logo} alt="logo"  />
-        </div>
-        <div className="nav-item">
-            <ul>
-                <li>About</li>
-                <li>How It Works</li>
-                <li>Pricing</li>
-            </ul>
-        </div>
-        <button className="login-button" type="submit">Login/SignUp</button>
-      </div>
-      <div className="Body">
-        <div className="background">
-          <img className="background1" src={Background} alt="background"></img>
-        </div>
-        <div className="center">
-          <div className="circle">
-            <div className="innerCircle">
-            </div>
-          </div>
-          
-        </div>
-
-      </div>
+    <Home1/>
     </>
-    
   )
 }
-export default Header
