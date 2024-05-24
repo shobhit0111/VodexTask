db6f1a445ca9763e8cddca3ee0e1ab4843c37649
shobhit0111 <shobhitbhatnagar61@gmail.com>
2024-05-25 03:55:28 +0530

    Home2Added

--- vodextask/src/App.js
+++ vodextask/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home1 from './Components/Home1/Home1'
+import Home2 from './Components/Home2/Home2'
 
 export default function App() {
   return (
     <>
-    <Home1/>
+    {/* <Home1/> */}
+    <Home2/>
     </>
   )
 }
